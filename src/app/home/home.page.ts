d78080a5767ce051fa4f77b8b625c13133e06064
DanielAntonio8a <145157623+DanielAntonio8a@users.noreply.github.com>
2024-03-15 16:25:09 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  temperatura: number=0;
+  mensaje: string=" ";
+  esFrio: boolean=false;
+  esTemplado: boolean=false;
+  esCalor: boolean=false;
+  escalas: number[] = [];
 
-  constructor() {}
-
+  calcularTemperatura() {
+    if(this.temperatura >101){
+      this.mensaje = "Temperatura no valida";
+      this.esFrio = true;
+      this.esTemplado = true;
+      this.esCalor = true;
+    } else if (this.temperatura < 10) {
+      this.mensaje = "Está haciendo frío.";
+      this.esFrio = true;
+      this.esTemplado = false;
+      this.esCalor = false;
+    } else if (this.temperatura >= 10 && this.temperatura < 25) {
+      this.mensaje = "Está templado.";
+      this.esFrio = false;
+      this.esTemplado = true;
+      this.esCalor = false;
+    } else {
+      this.mensaje = "Está haciendo calor.";
+      this.esFrio = false;
+      this.esTemplado = false;
+      this.esCalor = true;
+    }
+  }
+  calcularPosicionBola(): string {
+    const offset = (this.temperatura / 100) * 200; // Altura total del termómetro
+    return `${offset}px`; // Devuelve la posición en formato de cadena
+  }
+  
 }
